6b524c389e7ebfc54401d5d558d05891f921cc87
hepangda <pangda@xiyoulinux.org>
2018-07-24 11:03:01 +0800

    pangda's solution

--- Solution/pangda/1_J.cc
+++ Solution/pangda/1_J.cc
@@ -1,6 +1,25 @@
 #include <iostream>
 
-long factor_sum(int n)
+int factor_sum(int n)
 {
+    int sum = 0;
+    for (int i = n / 2; i >= 1; i--) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int main(int argc, char *argv[])
+{
+    int cas;
+    std::cin >> cas;
+
+    for (int a, b; cas--; ) {
+        std::cin >> a >> b;
+        std::cout << (a == factor_sum(b) && b == factor_sum(a) ? "YES" : "NO") << std::endl;
+    }
     
+    return 0;
 }
